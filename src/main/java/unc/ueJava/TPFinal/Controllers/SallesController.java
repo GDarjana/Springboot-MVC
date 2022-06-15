0893353b7363461c7c5d1ac644f208c1a600a94c
GDarjana <zarkashnc@yahoo.com>
2022-06-16 00:21:59 +1100

    MVC salle

---
+++ src/main/java/unc/ueJava/TPFinal/Controllers/SallesController.java
@@ -0,0 +1,85 @@
+package unc.ueJava.TPFinal.Controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import unc.ueJava.TPFinal.DAO.SalleRepository;
+import unc.ueJava.TPFinal.Model.Salle;
+
+@Controller
+public class SallesController {
+
+    @Autowired
+    private SalleRepository salleService;
+
+    @GetMapping("/salles")
+    public String salles(Model model) {
+        Iterable<Salle> liste_salle = salleService.findAll();
+        model.addAttribute("liste_salles", liste_salle);
+        return "salles_list";
+    }
+
+    /**
+     * Page d'ajout
+     * 
+     * @return
+     */
+    @GetMapping("/salles/salleForm")
+    public String ajouterSalle(Model model) {
+        Salle salle = new Salle();
+        model.addAttribute("salle", salle);
+        return "salle_form";
+    }
+
+    /*
+     * Page liste des salles après avoir ajouter une nouvelle salle
+     */
+    @PostMapping("/salles")
+    public String salles(@ModelAttribute("salle") Salle salle) {
+        salleService.save(salle);
+        return "redirect:/salles";
+    }
+
+    /*
+     * Page de modification d'une salle existante
+     */
+    @GetMapping("/salles/{id}/edit")
+    public String modifierSalle(@PathVariable("id") int id, Model model) {
+        Optional<Salle> salle = salleService.findById(id);
+        if (salle.isPresent()) {
+            model.addAttribute("salle", salle.get());
+            return "salle_update";
+        }
+        return "redirect:/salles";
+    }
+
+    /*
+     * Page de la liste des salles après avoir modifié une salle existante
+     */
+    @PostMapping("/salles/{id}/update")
+    public String modifierSalle(@PathVariable("id") int id, @Validated Salle salle, BindingResult result,
+            Model model) {
+        salle.setId(id);
+        salleService.save(salle);
+        model.addAttribute("liste_salles", this.salleService.findAll());
+        return "salles_list";
+    }
+
+    @GetMapping("/salles/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int id, Model model) {
+        Optional<Salle> salle = this.salleService.findById(id);
+        this.salleService.delete(salle.get());
+        model.addAttribute("liste_salles", this.salleService.findAll());
+        return "salles_list";
+    }
+
+}
--- src/main/java/unc/ueJava/TPFinal/Model/Salle.java
+++ src/main/java/unc/ueJava/TPFinal/Model/Salle.java
@@ -1,5 +1,66 @@
 package unc.ueJava.TPFinal.Model;
 
+import javax.persistence.*;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name = "salle")
+@DynamicUpdate
 public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "salleCode")
+    private String salleCode;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "capacite")
+    private int capacite;
+
+    public Salle() {
+    }
+
+    public Salle(String salleCode, String nom, int capacite) {
+        this.salleCode = salleCode;
+        this.nom = nom;
+        this.capacite = capacite;
+    }
+
+    public void setSalleCode(String salleCode) {
+        this.salleCode = salleCode;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    public String getSalleCode() {
+        return this.salleCode;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public int getCapacite() {
+        return this.capacite;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
