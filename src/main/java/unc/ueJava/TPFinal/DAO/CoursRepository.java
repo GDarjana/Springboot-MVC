9a55c24b2635fb42d79b8747d1e379c77785af7f
GDarjana <zarkashnc@yahoo.com>
2022-06-17 17:11:55 +1100

    Clean unused package

--- src/main/java/unc/ueJava/TPFinal/DAO/CoursRepository.java
+++ src/main/java/unc/ueJava/TPFinal/DAO/CoursRepository.java
@@ -4,9 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import unc.ueJava.TPFinal.Model.Cours;
