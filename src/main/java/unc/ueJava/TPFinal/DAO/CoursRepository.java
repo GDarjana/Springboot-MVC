2bac71d8828d117ea4318f865ea7fd67c5698bdf
GDarjana <zarkashnc@yahoo.com>
2022-06-17 12:24:07 +1100

    Ajout fonction findAllByNiveauId

--- src/main/java/unc/ueJava/TPFinal/DAO/CoursRepository.java
+++ src/main/java/unc/ueJava/TPFinal/DAO/CoursRepository.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +13,9 @@ import unc.ueJava.TPFinal.Model.Salle;
 @Repository
 public interface CoursRepository extends CrudRepository<Cours, Integer> {
 
-    Optional<Cours> findByDateDebutLessThanEqualAndDateFinGreaterThanEqualAndSalle(LocalDateTime dateFin, LocalDateTime dateDebut, Salle salle);
+    Optional<Cours> findByDateDebutLessThanEqualAndDateFinGreaterThanEqualAndSalle(LocalDateTime dateFin,
+            LocalDateTime dateDebut, Salle salle);
+
+    List<Cours> findAllByNiveauId(int niveau_id);
+
 }
